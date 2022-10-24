d2c2ac1c065bc88aa2476edd88c788b25bb31f80
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-10-24 10:43:19 +0200

    feat: first route to 'polls/'

--- intro_to_django/urls.py
+++ intro_to_django/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# from polls import urls
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
+    path('polls/', include('polls.urls')),
+    # path('polls/', include(urls)),
+    path('admin/', admin.site.urls)
 ]
